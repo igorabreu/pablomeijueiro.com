97ef96a680f878faa7d6bafaafe891e8971f8814
Igor Abreu <igor.abreu.reis@gmail.com>
2019-07-10 23:52:25 -0300

    feat: setup github pages

---
+++ docs/precache-manifest.5f3abf1309e883583fd9f600055bb659.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed5f72e0bb3ba3cc82aafff1d397977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8733e5dc6d4a33d9a3e3",
+    "url": "/static/js/2.0c252449.chunk.js"
+  },
+  {
+    "revision": "fe91a438539464bb9f0a",
+    "url": "/static/js/main.b30f6048.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b1335f1e1c7b37e3cdf7fa1f31222d5f",
+    "url": "/static/media/icon1.b1335f1e.svg"
+  },
+  {
+    "revision": "22365d5b5d07e3372e3e83c356fdad6f",
+    "url": "/static/media/icon2.22365d5b.svg"
+  },
+  {
+    "revision": "0c6b7c1167b40ee695e6624b1d811e75",
+    "url": "/static/media/icon3.0c6b7c11.svg"
+  },
+  {
+    "revision": "5285aa24f9eaf756779c39da65196802",
+    "url": "/static/media/icon4.5285aa24.svg"
+  }
+]);
